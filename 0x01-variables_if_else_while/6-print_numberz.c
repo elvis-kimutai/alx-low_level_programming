2be6f585c2100aa7195fffac8a3a2f9d62257533
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-02-18 12:03:33 -0800

    using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}

1b062f9aae4f37af381a9b91e677589e971b5885
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-03-13 06:37:10 -0700

    Write a function that creates an array of chars, and initializes it

---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * File: main.h
+ * author: elvis
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-malloc_free directory.
+ */
+
+int _putchar(char c);
+char *create_array(unsigned int size, char c);
+#endif

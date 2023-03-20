c4eeee55d60a7db492a4391001db688c848a8e67
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-03-20 09:27:56 -0700

    function like marco

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -3,7 +3,7 @@
 
 /*
  * File: 3-function_like_macro.h
- * Desc: Header file that defines a function-like macro ABS(x)
+ * Desc: Defines a function-like macro ABS(x)
  *       that computes the absolute value of a number x.
  */
 

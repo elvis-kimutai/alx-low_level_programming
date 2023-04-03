cb05edc4d42f52e49c2fa57d70352444b5a1938c
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-04-03 11:47:57 -0700

    List length

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -2,8 +2,9 @@
 #define LISTS_H
 
 /*
- * Descreption: Header file containing prototypes and definitions for all functions
- *       and types written in the 0x13-more_singly_linked_lists project.
+ * Descreption: Header file containing prototypes and
+ *	definitions for all functions
+ *	and types written in the 0x13-more_singly_linked_lists project.
  */
 
 #include <stdlib.h>
@@ -14,15 +15,15 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
- * 
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 int _putchar(char c);
 size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
 
 #endif /* LISTS_H */

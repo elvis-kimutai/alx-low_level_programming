041c8eed882df9edf8b67765bc6dbf3f9307dfe3
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-05-08 04:49:37 -0700

    prints POSIX standard output.

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <fcntl.h>
+
+/**
+ * read_textfile - reads a text file and prints it to POSIX stdout
+ * @filename: a pointer to the file name
+ * @letters: the number of letters the function should read and
+ * print
+ * Return: 0 (if the function fails or filename is NULL)
+ * otherwise - the actual number of files the function can
+ * read and print
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
+}

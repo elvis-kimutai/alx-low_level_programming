2f42e20047871be7356c6dfa512a3f537a06bceb
elvis-kimutai <cheruiyotkim17@gmail.com>
2023-10-11 06:48:04 -0700

    . Advanced binary search

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,7 +1,7 @@
 #include "search_algos.h"
 
 /**
- * binary_search2 - searches for a value in an array using BS
+ * _binary_search - searches for a value in an array using BS
  * @array: pointer to the array to search
  * @left:  beginnig of an array
  * @right: end of an array
---
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -0,0 +1,46 @@
+#include "search_algos.h"
+
+/**
+ * binary_recursion - searches for a value recursively
+ * @array: array to search
+ * @left: begining of an array/sub-array - index
+ * @right: end of an array/subarray = index
+ * @value: value of an array to search for
+ * Return: index of the found value, -1 if not found
+ */
+int advanced_binary_recursive(int *array, size_t left, size_t right, int value)
+{
+	size_t i;
+
+	if (right < left)
+		return (-1);
+
+	printf("Searching in array: ");
+	for (i = left; i < right; i++)
+		printf("%d, ", array[i]);
+	printf("%d\n", array[i]);
+
+	i = left + (right - left) / 2;
+	if (array[i] == value && (i == left || array[i - 1] != value))
+		return (i);
+	if (array[i] >= value)
+		return (advanced_binary_recursive(array, left, i, value));
+	return (advanced_binary_recursive(array, i + 1, right, value));
+}
+
+
+/**
+ * advanced_binary - searches a value in a sorted arr of ints
+ * @array: array to search for the value
+ * @size: size of the array to search
+ * @value: value to search for
+ * Return: index of the searched value
+ */
+
+int advanced_binary(int *array, size_t size, int value)
+{
+	if (array == NULL || size == 0)
+		return (-1);
+
+	return (advanced_binary_recursive(array, 0, size - 1, value));
+}
